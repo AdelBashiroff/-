fcf2fe6e7300a723ffc32cd7208ea94045dd9427
AdelBashiroff <okblyau@gmail.com>
2025-06-10 20:40:51 +0300

    Add files via upload

---
+++ ClassWork8__task1.py
@@ -0,0 +1,44 @@
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __str__(self):
+        return f'Название: {self.title}, Автор: {self.author}, Год: {self.year}'
+    
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        if isinstance(book, Book):
+            self.books.append(book)
+            print(f'Книга {book.title} успешно добавлена')
+        else:
+            print('Можно добавлять только объекты класса Book')
+
+    def find_books_by_author(self, author):
+        found_books = []
+        for book in self.books:
+            if book.author == author:
+                found_books.append(book)
+        return found_books
+
+    def find_books_by_year(self, year):
+        found_books = []
+        for book in self.books:
+            if book.year == year:
+                found_books.append(book)
+        return found_books
+
+library = Library()
+book1 = Book('1984', 'Джордж Оруэлл', 1949)
+book2 = Book('Война и мир', 'Лев Толстой', 1867)
+
+library.add_book(book1)
+library.add_book(book2)
+
+books = library.find_books_by_author('Лев Толстой')
+for book in books:
+    print(book)
